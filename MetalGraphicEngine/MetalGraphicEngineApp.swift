decf480a4f90d42921219548f9ddfeb953e55c40
Ertem Biyik <ertembiyik@gmail.com>
2022-06-04 21:45:03 +0300

    Initial Commit

---
+++ MetalGraphicEngine/MetalGraphicEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalGraphicEngineApp.swift
+//  MetalGraphicEngine
+//
+//  Created by Ertem Biyik on 04.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MetalGraphicEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
