0c7d56a0563013349e6ac923eff1bf1405508e1f
Ertem Biyik <ertembiyik@gmail.com>
2022-06-08 15:27:27 +0300

    fixes

---
+++ Shared/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  InverseTriangulation
+//
+//  Created by Ertem Biyik on 08.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          MetalView()
+            .border(Color.black, width: 2)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
